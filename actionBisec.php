5a73cfbfd14dc144c3619d27daf9ee2e74c24a92
Rikardo816 <rikardo_t@hotmail.es>
2022-06-26 22:59:14 -0500

    update: styles in bisecccion method

--- actionBisec.php
+++ actionBisec.php
@@ -1,5 +1,28 @@
 <?php
-    namespace MathPHP\NumericalAnalysis\RootFinding;
+namespace MathPHP\NumericalAnalysis\RootFinding;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Método de Bisección</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manjari&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<header>
+        <h1>Métodos para encontrar raíces con PHP</h1>
+    </header>
+    <div id="container">
+        <div id="centro">
+<?php
+    // namespace MathPHP\NumericalAnalysis\RootFinding;
 
     require('BisectionMethod.php');
 
@@ -24,3 +47,10 @@
 
     echo "Resultado: " . $resultado;
 ?>
+</div>
+    <footer>
+            Metodos Numéricos 2022A
+        </footer>
+</div>
+</body>
+</html>
